1cd42ca7670212ef6917a0221cca9d273602cf7b
Yasin <yasinabdussaburr@gmail.com>
2021-09-25 19:54:51 -0400

    commit tree 2e849eae4c5b7daed9d8b9d9239c0e03ad161dea

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+const EyesOnMe = () => {
+   const eventHandler = (e) => {
+       console.log('Good!')
+   }
+   const eventBlur = (e) => {
+       console.log('Hey! Eyes on me!')
+}
+    return(
+        <>
+        <button onFocus={eventHandler} onBlur={eventBlur}>
+            'Eyes on me'
+            </button>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import react from "react";
+
+const Keypad = () => {
+    const eventHandler = (e) => {
+        console.log('Entering password...')
+    }
+    return (
+        <div>
+        <input type="password" onChange={eventHandler} ></input>
+        </div>
+    )
+}
+
+export default Keypad;
